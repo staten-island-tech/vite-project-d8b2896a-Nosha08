f24cf921f5582e2711364ebf16b178e3d4ef64ec
Nosha08 <noah.rozin@outlook.com>
2023-11-07 22:12:46 -0500

    easy

---
+++ vite-project/js/array.js
@@ -0,0 +1,44 @@
+const realEstate = [
+    {
+        name: 'House 1',
+        placeholder: true
+    },
+    {
+        name: 'House 2',
+        placeholder: true
+    },
+    {
+        name: 'House 3',
+        placeholder: true
+    },
+    {
+        name: 'House 4',
+        placeholder: true
+    },
+    {
+        name: 'House 5',
+        placeholder: true
+    },
+    {
+        name: 'House 6',
+        placeholder: true
+    },
+    {
+        name: 'House 7',
+        placeholder: true
+    },
+    {
+        name: 'House 8',
+        placeholder: true
+    },
+    {
+        name: 'House 9',
+        placeholder: true
+    },
+    {
+        name: 'House 10',
+        placeholder: false
+    },
+]
+
+export {realEstate}
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,12 +1,44 @@
-import '../styles/style.css'
+import '../styles/style.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import {realEstate} from './array.js'
 import javascriptLogo from '../javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from '../counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hey</h1>
-  </div>
-`
+AOS.init()
+
+const DOMSelectors = {
+  display: document.querySelector('.display'),
+  button1: document.querySelector('.buttonAll'),
+  button2: document.querySelector('.buttonTest'),
+}
+
+DOMSelectors.button1.addEventListener('click', function () {
+  const div = DOMSelectors.display;
+  remove(div)
+  realEstate.forEach((property) => {
+    DOMSelectors.display.insertAdjacentHTML('beforeend', `
+      <h1>${property.name}</h1>
+    `)
+  })
+})
+
+DOMSelectors.button2.addEventListener('click', function () {
+  const div = DOMSelectors.display;
+  remove(div)
+  const a = realEstate.filter((x) => x.placeholder === true)
+  a.forEach((property) => {
+    DOMSelectors.display.insertAdjacentHTML('beforeend', `
+      <h1>${property.name}</h1>
+    `)
+  })
+})
+
+function remove(div) {
+  div.innerHTML = ''
+}
 
 setupCounter(document.querySelector('#counter'))
+
+console.log(realEstate)
