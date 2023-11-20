eb1f45b754b338cb9565918c1e74900c515ca245
Nosha08 <noah.rozin@outlook.com>
2023-11-19 21:01:18 -0500

    a

--- vite-project/js/array.js
+++ vite-project/js/array.js
@@ -3,74 +3,88 @@
 const realEstate = [
     {
         name: "Shrek's Shack",
-        location: '',
-        description: 'Located in t',
+        location: 'Swampy Meadows, Far Far Away',
+        description: 'Quaint cottage in the heart of Shrek\'s swamp.',
         price: 0.03,
         EvanCanAfford: false
     },
     {
         name: 'The White House',
-        location: '',
-        description: 'Located in t',
+        location: '1600 Pennsylvania Avenue NW, Washington, D.C.',
+        description: 'Historic residence of the President of the United States.',
         price: 45000000,
         EvanCanAfford: false
     },
     {
         name: 'Skid Row Tent',
-        location: '',
-        description: 'Located in t',
+        location: 'Skid Row, Downtown Los Angeles',
+        description: 'Cozy tent in the vibrant Skid Row neighborhood.',
         price: 0,
         EvanCanAfford: true
     },
     {
         name: "Spongebob's Pineapple",
-        location: '',
-        description: 'Located in t',
+        location: 'Bikini Bottom, Pacific Ocean',
+        description: 'Charming pineapple-shaped home under the sea.',
         price: 15,
         EvanCanAfford: false
     }, 
     {
         name: 'Palace of Versailles',
-        location: '',
-        description: 'Located in t',
+        location: 'Versailles, France',
+        description: 'Opulent palace known for its stunning architecture and gardens.',
         price: 0,
         EvanCanAfford: false
     },
     {
         name: 'Mickey Mouse Clubhouse',
-        location: '',
-        description: 'Located in t',
+        location: 'Disneyland, Anaheim, California',
+        description: 'Iconic clubhouse in the magical world of Disney.',
         price: 0,
         EvanCanAfford: false
     },
     {
         name: 'The Biltmore',
-        location: '',
-        description: 'Located in t',
+        location: 'Asheville, North Carolina',
+        description: 'Elegant mansion with a rich history and beautiful grounds.',
         price: 0,
         EvanCanAfford: false
     },
     {
         name: 'Hong Kong Apartment',
-        location: '',
-        description: 'Located in t',
+        location: 'Victoria Peak, Hong Kong',
+        description: 'Modern apartment with a stunning view of the Hong Kong skyline.',
         price: 0,
         EvanCanAfford: false
     },
     {
         name: "Kim Jung Un's Estate",
-        location: '',
-        description: 'Located in t',
+        location: 'Ryongsong Residence, Pyongyang, North Korea',
+        description: 'Expansive estate belonging to the Supreme Leader of North Korea.',
         price: 0,
         EvanCanAfford: false
     },
     {
         name: "Peter Griffin's home",
-        location: '',
-        description: 'Located in t',
+        location: 'Quahog, Rhode Island',
+        description: 'Quirky family home in the animated town of Quahog.',
         price: 0,
         EvanCanAfford: false
     },
-]
+    {
+        name: 'Beachfront Villa',
+        location: 'Malibu, California',
+        description: 'Luxurious villa with direct access to the sandy beaches of Malibu.',
+        price: 2000000,
+        EvanCanAfford: false
+    },
+    {
+        name: 'Mountain Retreat',
+        location: 'Aspen, Colorado',
+        description: 'Secluded retreat nestled in the scenic mountains of Aspen.',
+        price: 1500000,
+        EvanCanAfford: false
+    },
+];
 
 export {realEstate}
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -8,16 +8,22 @@ AOS.init()
 const DOMSelectors = {
   display: document.querySelector('.display'),
   button1: document.querySelector('.buttonAll'),
-  button2: document.querySelector('.buttonTest'),
+  button2: document.querySelector('.buttonAfford'),
+  button3: document.querySelector('.buttonTest')
 }
 
 DOMSelectors.button1.addEventListener('click', function () {
   const div = DOMSelectors.display;
   remove(div)
+
   realEstate.forEach((property) => {
     DOMSelectors.display.insertAdjacentHTML('beforeend', `
     <div class='card'>
       <h1>${property.name}</h1>
+      <h2>${property.location}</h2>
+      <p>${property.description}</p>
+      <h2>Evan can afford: ${property.EvanCanAfford}</h2>
+      <h1>$${property.price}</h2>
     </div>
     `)
   })
@@ -26,14 +32,40 @@ DOMSelectors.button1.addEventListener('click', function () {
 DOMSelectors.button2.addEventListener('click', function () {
   const div = DOMSelectors.display;
   remove(div)
-  const a = realEstate.filter((x) => x.EvanCanAfford === true)
-  a.forEach((property) => {
+
+  const affordable = realEstate.filter((x) => x.EvanCanAfford === true)
+  affordable.forEach((property) => {
     DOMSelectors.display.insertAdjacentHTML('beforeend', `
     <div class='card'>
       <h1>${property.name}</h1>
+      <h2>${property.location}</h2>
+      <p>${property.description}</p>
+      <h2>Evan can afford: ${property.EvanCanAfford}</h2>
+      <h1>$${property.price}</h2>
     </div>
     `)
   })
+})
+
+DOMSelectors.button3.addEventListener('click', function () {
+  const div = DOMSelectors.display;
+  remove(div);
+
+  const inflatedPropertyHTML = realEstate.map((property) => {
+    const inflatedPrice = property.price * 10; // Simulating 10x price inflation
+
+    return `
+      <div class='card'>
+        <h1>${property.name}</h1>
+        <h2>${property.location}</h2>
+        <p>${property.description}</p>
+        <h2>Evan can afford: ${property.EvanCanAfford}</h2>
+        <h1>$${inflatedPrice}</h2> <!-- Displaying the inflated price -->
+      </div>
+    `;
+  }).join('');
+
+  DOMSelectors.display.insertAdjacentHTML('beforeend', inflatedPropertyHTML);
 })
 
 function remove(div) {
